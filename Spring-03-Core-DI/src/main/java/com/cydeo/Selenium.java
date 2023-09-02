d8fa81b0babdc5fbfa2274e21cbd9063c832b58c
kvadrat-13 <edilbek.browne@gmail.com>
2023-09-02 02:22:30 -0400

    Dependency Injection

---
+++ Spring-03-Core-DI/src/main/java/com/cydeo/Selenium.java
@@ -0,0 +1,9 @@
+package com.cydeo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Selenium {
+    public void getTeachingHours(){
+        System.out.println("Total teaching hours : 15");
+    }}
