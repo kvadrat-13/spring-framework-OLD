789ccdbef9741e1a55115218dc238beafef97382
kvadrat-13 <edilbek.browne@gmail.com>
2023-09-02 00:37:18 -0400

    bean annotation

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/ConfigApp.java
@@ -0,0 +1,17 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigApp {
+    @Bean
+    FullTimeMentor fullTimeMentor(){
+     return new FullTimeMentor();
+    }
+    @Bean
+    PartTimeMentor partTimeMentor(){
+        return new PartTimeMentor();
+    }
+
+}
